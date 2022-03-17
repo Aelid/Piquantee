54d936922f6cfb4252949c2a6799ef31cc906240
Aelid <85112285+Aelid@users.noreply.github.com>
2022-03-17 04:40:42 +0100

    update

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -1,22 +1,20 @@
 const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv").config();
 
+// Middleware pour verifier si l'utilisateur est connecté
 module.exports = (req, res, next) => {
   try {
-    if (!req.headers.authorization) {
-      // on verifie l'autorisation 
-      throw "Merci de vous connecter";
-    }
-
-    const token = req.headers.authorization.split(" ")[1]; // on trouve la partie du token authorization headers
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_TOKEN); // on regarde si ça correspond a la secret token key
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
     const userId = decodedToken.userId;
+    req.auth = { userId };
     if (req.body.userId && req.body.userId !== userId) {
-      throw "userId non valable !";
+      throw "Invalid user ID !";
     } else {
       next();
     }
-  } catch (error) {
-    res.status(401).json({ error: error || "Requête non authentifiée !" });
+  } catch {
+    res.status(401).json({
+      error: new Error("Invalid request !"),
+    });
   }
 };
