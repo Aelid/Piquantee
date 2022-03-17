98afdeae8cabccaeb729a88b730b3632b0da6131
Aelid <85112285+Aelid@users.noreply.github.com>
2022-03-17 04:41:38 +0100

    update

--- back/server.js
+++ back/server.js
@@ -1,8 +1,6 @@
-
 const http = require("http");
 const app = require("./app");
 
-//  configuration des ports
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
 
@@ -14,18 +12,15 @@ const normalizePort = (val) => {
   }
   return false;
 };
-
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
-// gestion erreur
 const errorHandler = (error) => {
   if (error.syscall !== "listen") {
     throw error;
   }
   const address = server.address();
-  const bind =
-    typeof address === "string" ? "pipe " + address : "port: " + port;
+  const bind = typeof address === "string" ? "pipe " + address : "port: " + port;
   switch (error.code) {
     case "EACCES":
       console.error(bind + " requires elevated privileges.");
@@ -40,12 +35,13 @@ const errorHandler = (error) => {
   }
 };
 
-// redirection vers l'express app
 const server = http.createServer(app);
+
 server.on("error", errorHandler);
 server.on("listening", () => {
   const address = server.address();
   const bind = typeof address === "string" ? "pipe " + address : "port " + port;
-  console.log("Listening on " + bind);
+  console.log("Listening on " + bind + " ! 👂 ⚡");
 });
+
 server.listen(port);
